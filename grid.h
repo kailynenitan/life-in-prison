91403478ae2f292a7a96e1391d221d674228f9a0
kailynenitan <kailyn.enitan@gmail.com>
2025-07-30 04:48:50 +0000

    Created grid class.

--- grid.h
+++ grid.h
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <vector>
 
 #ifndef GRID_H_INCLUDED
 #define GRID_H_INCLUDED
+
+class grid {
+public:
+  grid(int rows, int cols) {
+    m_rows = rows;
+    m_cols = cols;
+
+    // Fill entire grid with dead cells
+    for (int r = 0; r < m_rows; ++r) {
+      std::vector<cell> append_row;
+      for (int c = 0; c < m_cols; ++c) {
+        append_row.push_back(cell(false, r, c));
+      }
+
+      m_grid.push_back(append_row);
+    }
+
+    return;
+  }
+
+  ~grid() {}
+
+  // Print entire grid.
+  // For every cell printed, "O" means alive and "." means dead
+  void print() {
+    for (int r = 0; r < this->m_rows; ++r) {
+      for (int c = 0; c < this->m_cols; ++c) {
+        (this->m_grid[r][c].state()) ? std::cout << "O" : std::cout << ".";
+      }
+      std::cout << "\n";
+    }
+
+    return;
+  }
+
+private:
+  int m_rows;
+  int m_cols;
+  std::vector<std::vector<cell>> m_grid;
+};
 
 class cell {
 public:
@@ -15,6 +56,18 @@ public:
     std::cout << this->m_alive << ".\n";
     return;
   }
+
+  void alive() {
+    this->m_alive = true;
+    return;
+  }
+
+  void dead() {
+    this->m_alive = false;
+    return;
+  }
+
+  bool state() { return this->m_alive; }
 
 private:
   bool m_alive;
