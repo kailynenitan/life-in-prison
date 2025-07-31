6ff5a36659b4f42d4046331637d46e38d012235a
kailynenitan <kailyn.enitan@gmail.com>
2025-07-31 04:40:33 +0000

    Marked some cells as alive. All neighbors of (1,1) are alive.

--- grid.cpp
+++ grid.cpp
@@ -2,6 +2,16 @@
 
 int main() {
   grid test(10, 10);
+
+  test.mark(0, 0, true);
+  test.mark(0, 1, true);
+  test.mark(0, 2, true);
+  test.mark(1, 0, true);
+  test.mark(1, 2, true);
+  test.mark(2, 0, true);
+  test.mark(2, 1, true);
+  test.mark(2, 2, true);
+
   test.print();
   return 0;
 }
