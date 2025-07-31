e5e1738658a49f4b59a85a4741ff180d53b6748d
kailynenitan <kailyn.enitan@gmail.com>
2025-07-31 06:18:48 +0000

    Altered grid.cpp to test the populate member function in the grid class.

--- grid.cpp
+++ grid.cpp
@@ -2,29 +2,10 @@
 #include <iostream>
 
 int main() {
-  grid test(10, 10);
+  grid testgrid(10, 10);
 
-  test.mark(0, 0, true);
-  test.mark(0, 1, true);
-  test.mark(0, 2, true);
-  test.mark(1, 0, true);
-  test.mark(1, 2, true);
-  test.mark(2, 0, true);
-  test.mark(2, 1, true);
-  test.mark(2, 2, true);
+  testgrid.populate("input.txt");
 
-  auto num_alive = [&test](int row, int col) {
-    std::cout << "Number of cells alive at (" << row << ", " << col << "): ";
-    std::cout << test.census(row, col) << std::endl;
-  };
-
-  num_alive(1, 1);
-  num_alive(1, 2);
-  num_alive(0, 0);
-  num_alive(9, 9);
-  num_alive(0, 9);
-  num_alive(9, 0);
-
-  test.print();
+  testgrid.print();
   return 0;
 }
