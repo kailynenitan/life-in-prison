ab3876a9870e065c679f1d8e450eb72948e5643d
kailynenitan <kailyn.enitan@gmail.com>
2025-08-10 07:43:51 +0000

    Deleted cell class from these files.

--- grid.h
+++ grid.h
@@ -5,29 +5,6 @@
 #include <string>
 #include <vector>
 
-class cell {
-    public:
-    cell(bool alive, int row, int col);
-
-    ~cell();
-
-    void print_state();
-
-    void alive();
-
-    void dead();
-
-    bool state();
-
-    void update_state();
-
-    private:
-    bool m_alive;
-    bool m_new_state;
-    int m_row;
-    int m_col;
-};
-
 class grid {
     public:
     grid(int rows, int cols);
